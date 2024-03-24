f041cfb5a51fad66c68fbaa0595e918e71aed5b5
Rodrigo Viana <rodrigo.viana@tseconsulting.com.br>
2024-03-24 18:03:49 -0300

    first commit

---
+++ src/components/LinkedInBanner.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const LinkedInBanner = () => (
+  <div className='banner'>
+    <div>
+      <img src={myPhoto} alt="profile"/>
+    </div>
+    <div>
+      <p>
+        Contact me: +55 35 998664575
+      </p>
+    </div>
+  </div>
+)
+
+export default LinkedInBanner
